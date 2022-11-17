126e8c7100f2dbba01b08192f925953899fe8274
johnson <johnson.dev298@gmail.com>
2022-11-17 15:45:28 +0700

    update ui create land

--- src/pages/portal/new.tsx
+++ src/pages/portal/new.tsx
@@ -1,9 +1,10 @@
 import React, {useState} from "react";
-import { Typography, Button, Divider, Form, Input, Upload, message } from 'antd';
+import { Typography, Button, Divider, Form, Input, Upload, message, Row, Col } from 'antd';
 const { Title } = Typography;
 import type { RcFile, UploadFile, UploadProps } from 'antd/es/upload/interface';
 import type { UploadChangeParam } from 'antd/es/upload';
 import { LoadingOutlined, PlusOutlined, UploadOutlined } from '@ant-design/icons';
+import {onChangePrice, validateEmpty, validateIsNumber} from "../../utils/validate.util";
 
 const getBase64 = (img: RcFile, callback: (url: string) => void) => {
     const reader = new FileReader();
@@ -23,10 +24,11 @@ const beforeUpload = (file: RcFile) => {
     return isJpgOrPng && isLt2M;
 };
 
-const NewLandPage: React.FC = () => {
+const NewLandPage: React.FC<any> = (props) => {
     const [loading, setLoading] = useState(false);
     const [imageUrl, setImageUrl] = useState<string>();
     const [projectImgList, setProjectImgList] = useState<UploadFile[]>([]);
+    const {getFieldDecorator} = props
 
     const propPaper: UploadProps = {
         onChange({ file, fileList }) {
@@ -86,98 +88,130 @@ const NewLandPage: React.FC = () => {
         </div>
     );
 
+    const [form] = Form.useForm()
+
+
     return (
         <>
             <Title level={2}>Request Form To NFT Your LAND</Title>
             <div className='box'>
                 <Form
-                    labelCol={{ span: 4 }}
-                    wrapperCol={{ span: 20 }}
+                    form={form}
+                    labelCol={{ span: 24 }}
+                    wrapperCol={{ span: 24 }}
                     onFinish={onFinish}
                     onFinishFailed={onFinishFailed}
                     autoComplete="off"
                 >
-                    <Form.Item
-                        label="Avatar"
-                        name="avatar"
-                        rules={[{ required: true, message: 'Please upload avatar!' }]}
-                    >
-                        <Upload
-                            name="avatar"
-                            listType="picture-card"
-                            className="avatar-uploader"
-                            showUploadList={false}
-                            beforeUpload={beforeUpload}
-                            onChange={handleChange}
-                        >
-                            {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
-                        </Upload>
-                    </Form.Item>
-                    <Form.Item
-                        label="Address"
-                        name="address"
-                        rules={[{ required: true, message: 'This field cannot be empty.' }]}
-                    >
-                        <Input placeholder='Input Address'/>
-                    </Form.Item>
+                    <Row gutter={[40,20]}>
+                        <Col span={12}>
+                            <Form.Item
+                                label="Avatar"
+                                name="avatar"
+                                rules={[{ required: true, message: 'Please upload avatar!' }]}
+                            >
+                                <Upload
+                                    name="avatar"
+                                    listType="picture-card"
+                                    className="avatar-uploader"
+                                    showUploadList={false}
+                                    beforeUpload={beforeUpload}
+                                    onChange={handleChange}
+                                >
+                                    {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+                                </Upload>
+                            </Form.Item>
+                            <Form.Item
+                                label="Address"
+                                name="address"
+                                rules={[{ required: true, message: 'This field cannot be empty.' }]}
+                            >
+                                <Input placeholder='Input Address'/>
+                            </Form.Item>
 
-                    <Form.Item
-                        label="Token Name"
-                        name="tokenName"
-                        rules={[{ required: true, message: 'This field cannot be empty.' }]}
-                    >
-                        <Input placeholder='Input Token Name'/>
-                    </Form.Item>
+                            <Form.Item
+                                label="Token Name"
+                                name="tokenName"
+                                rules={[{ required: true, message: 'This field cannot be empty.' }]}
+                            >
+                                <Input placeholder='Input Token Name'/>
+                            </Form.Item>
 
-                    <Form.Item
-                        label="Total Supply"
-                        name="totalSupply"
-                        rules={[{ required: true, message: 'This field cannot be empty.' }]}
-                    >
-                        <Input placeholder='Input Total Supply'/>
-                    </Form.Item>
+                            <Form.Item
+                                required={true}
+                                label="Total Supply"
+                                name="totalSupply"
+                                rules={[
+                                    {
+                                        validator: async (_, value) => {
+                                            await validateEmpty(value);
+                                        }
+                                    }
+                                ]}
+                            >
+                                <Input
+                                    onChange={ async (e: any) => {
+                                        onChangePrice(e.target.value, form, "totalSupply")
+                                    }}
+                                    placeholder='Input Total Supply'/>
+                            </Form.Item>
 
-                    <Form.Item
-                        label="Token Price"
-                        name="tokenPrice"
-                        rules={[{ required: true, message: 'This field cannot be empty.' }]}
-                    >
-                        <Input placeholder='Input Token Price'/>
-                    </Form.Item>
+                            <Form.Item
+                                required={true}
+                                label="Token Price"
+                                name="tokenPrice"
+                                rules={[
+                                    {
+                                        validator: async (_, value) => {
+                                            await validateEmpty(value);
+                                        }
+                                    }
+                                ]}
+                            >
+                                <Input
+                                    onChange={ async (e: any) => {
+                                        onChangePrice(e.target.value, form, "tokenPrice")
+                                    }}
+                                    placeholder='Input Token Price'/>
+                            </Form.Item>
+                        </Col>
+                        <Col span={12}>
 
-                    <Form.Item
-                        label="Project Description"
-                        name="description"
-                        rules={[{ required: true, message: 'This field cannot be empty.' }]}
-                    >
-                        <Input.TextArea placeholder='Input Description' rows={6} />
-                    </Form.Item>
+                            <Form.Item
+                                label="Project Description"
+                                name="description"
+                                rules={[{ required: true, message: 'This field cannot be empty.' }]}
+                            >
+                                <Input.TextArea placeholder='Input Description' rows={6} />
+                            </Form.Item>
 
-                    <Divider orientation="center" orientationMargin="0">Project Images</Divider>
-                    <Form.Item
-                        label="Images"
-                        name="projectImages"
-                        rules={[{ required: true, message: 'This field cannot be empty.' }]}
-                    >
-                        <Upload
-                            listType="picture-card"
-                            fileList={projectImgList}
-                            onChange={onChangeProjectImages}
-                            onPreview={onPreview}
-                        >
-                            {projectImgList.length < 5 && '+ Upload'}
-                        </Upload>
-                    </Form.Item>
-                    <Divider orientation="center" orientationMargin="0">Legal Papers</Divider>
-                    <Form.Item
-                        label="Docs"
-                        name="LegalPapers"
-                        rules={[{ required: true, message: 'This field cannot be empty.' }]}
-                    >
-                        <Upload {...propPaper}>
-                            <Button icon={<UploadOutlined />}>Upload</Button>
-                        </Upload>
-                    </Form.Item>
+                            <Divider orientation="center" orientationMargin="0">Project Images</Divider>
+                            <Form.Item
+                                label="Images"
+                                name="projectImages"
+                                rules={[{ required: true, message: 'This field cannot be empty.' }]}
+                            >
+                                <Upload
+                                    listType="picture-card"
+                                    fileList={projectImgList}
+                                    onChange={onChangeProjectImages}
+                                    onPreview={onPreview}
+                                >
+                                    {projectImgList.length < 5 && '+ Upload'}
+                                </Upload>
+                            </Form.Item>
+                            <Divider orientation="center" orientationMargin="0">Legal Papers</Divider>
+                            <Form.Item
+                                label="Docs"
+                                name="LegalPapers"
+                                rules={[{ required: true, message: 'This field cannot be empty.' }]}
+                            >
+                                <Upload {...propPaper}>
+                                    <Button icon={<UploadOutlined />}>Upload</Button>
+                                </Upload>
+                            </Form.Item>
+                        </Col>
+                    </Row>
 
                     <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
                         <Button type="primary" htmlType="submit">
