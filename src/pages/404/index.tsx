710cf7d2e944311c311a042dc7144ae20b1708fb
johnson <johnson.dev298@gmail.com>
2022-11-23 17:45:11 +0700

    fix build

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,9 +1,16 @@
-import Link from "next/link";
+import React from "react";
+import Link from "next/link"
+import {ArrowLeftOutlined} from "@ant-design/icons";
 
-export default function FourOhFour() {
-  return (
-    <>
-      <h1>404 - Page Not Found</h1>
-    </>
-  );
+const NotFoundPage:React.FC = () => {
+    return (
+        <div className="fof">
+            <h1>Error 404</h1>
+            <Link href="/">
+                <ArrowLeftOutlined/> Back to home page
+            </Link>
+        </div>
+    )
 }
+
+export default NotFoundPage
