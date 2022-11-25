d4c59cfb28eacce143b4fee52a4986b0abc0c6cc
Sotatek-TrangTrinh <trang.trinh@sotatek.com>
2022-11-25 09:30:40 +0700

    fix baseinterface

--- src/programs/interfaces/BaseInterface.ts
+++ src/programs/interfaces/BaseInterface.ts
@@ -1,18 +1,14 @@
 import { Program, AnchorProvider } from "@project-serum/anchor";
 export default class BaseInterface {
-	_provider: AnchorProvider;
-	_programAddress: string;
-	_idl: any;
-	_program: Program;
+  _provider: AnchorProvider;
+  _programAddress: string;
+  _idl: any;
+  _program: Program;
 
-	constructor(
-		provider: AnchorProvider,
-		programAddress: string,
-		idl: any
-	) {
-		this._provider = provider;
-		this._programAddress = idl.metadata.address;
-		this._idl = idl;
-		this._program = new Program(idl, programAddress, provider);
-	}
+  constructor(provider: AnchorProvider, programAddress: string, idl: any) {
+    this._provider = provider;
+    this._programAddress = programAddress;
+    this._idl = idl;
+    this._program = new Program(idl, programAddress, provider);
+  }
 }
