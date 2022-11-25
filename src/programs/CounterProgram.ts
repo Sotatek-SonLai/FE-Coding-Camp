a2a439a49bc6d6c5e817662411160d56a72626b6
philipp-do <quang.do2@sotatek.com>
2022-11-25 09:45:31 +0700

    Fix build

--- src/programs/CounterProgram.ts
+++ src/programs/CounterProgram.ts
@@ -5,7 +5,7 @@ import { web3, BN, AnchorProvider } from "@project-serum/anchor";
 export default class CounterProgram extends BaseInterface {
   _counter: web3.Keypair;
   constructor(provider: AnchorProvider) {
-    super(provider, getCounterIdl());
+    super(provider, getCounterIdl().metadata.address, getCounterIdl());
     this._counter = web3.Keypair.generate();
   }
 
