e3e93049b6a08116d2cf4098bb7b08778b902dd4
johnson <johnson.dev298@gmail.com>
2022-11-29 16:06:53 +0700

    updated

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -13,29 +13,6 @@ import MainLayout from "../../components/Main-Layout";
 import { NextPageWithLayout } from "../_app";
 const { Title } = Typography;
 
-const requestAssetData: RequestAssetDataType[] = [
-    {
-        id: "1",
-        propertyInfo: "https://joeschmoe.io/api/v1/random",
-        name: "ABC",
-        totalSupply: 100000,
-        tokenPrice: 0.0001,
-        status: "pending",
-        action: "Fractionalize",
-        detail: "Detail",
-    },
-    {
-        id: "2",
-        propertyInfo: "https://joeschmoe.io/api/v1/random",
-        name: "Trinh Thi Thu Trang Trinh Thi Thu Trang Trinh Thi Thu Trang",
-        totalSupply: 1000000000,
-        tokenPrice: 0.001,
-        status: "rejected",
-        action: "Fractionalize",
-        detail: "Detail",
-    },
-];
-
 const passedAssetData: PassedAssetDataType[] = [
     {
         id: "1",
@@ -62,50 +39,15 @@ const passedAssetData: PassedAssetDataType[] = [
 ];
 
 const PortalPage:NextPageWithLayout = (props: any) => {
-    const { requestAssetData } = props;
-    const items = [
-        {
-            label: "Request Asset",
-            key: "request_asset",
-            children: (
-                <>
-                    {requestAssetData && (
-                        <Table
-                            dataSource={requestAssetData}
-                            columns={requestAssetColumns}
-                            rowKey="id"
-                            pagination={{ pageSize: 6 }}
-                        />
-                    )}
-                </>
-            ),
-        },
-        {
-            label: "Passed Asset",
-            key: "passed_asset",
-            children: (
-                <Table
-                    dataSource={passedAssetData}
-                    columns={passedAssetColumns}
-                    rowKey="id"
-                />
-            ),
-        },
-    ];
 
     return (
         <div>
             <Title level={2}>Portfolio</Title>
-            <div>
-                <Space style={{ width: "100%" }} direction="vertical" align="end">
-                    <Link href="/portal/new" passHref>
-                        <Button type="primary" icon={<PlusOutlined />}>
-                            Create New Land
-                        </Button>
-                    </Link>
-                </Space>
-            </div>
-            <Tabs items={items} />
+            <Table
+                dataSource={passedAssetData}
+                columns={passedAssetColumns}
+                rowKey="id"
+            />
         </div>
     );
 };
@@ -115,12 +57,3 @@ PortalPage.getLayout = (page: ReactElement) => {
 };
 
 export default PortalPage;
-
-export async function getStaticProps(context: any) {
-    const [res]: any = await EvaluationService.getLand();
-    return {
-        props: {
-            requestAssetData: res,
-        }, // will be passed to the page component as props
-    };
-}
